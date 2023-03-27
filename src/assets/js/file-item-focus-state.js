d79f8ed28400a4b76d51fdd6dda2ce2d54b10a5c
RoqueC <roquecarrizof@gmail.com>
2023-03-27 20:22:23 +0200

    Fix focus state flicker

--- src/assets/js/file-item-focus-state.js
+++ src/assets/js/file-item-focus-state.js
@@ -1,22 +1,36 @@
+initializeFocus();
+
 window.addEventListener("load", updateFocus);
 window.addEventListener("beforeunload", saveFocus);
 
 function saveFocus() {
-  const fileItemElements = document.getElementsByClassName("file-item");
-  let focusIndex = -1;
-  for (let i = 0; i < fileItemElements.length; i++) {
-    if (document.activeElement === fileItemElements[i].children[0]) {
-      focusIndex = i;
-    }
+  console.log("ttest");
+  const focusedElement = document.activeElement.parentElement;
+  if (focusedElement.classList.contains("file-item")) {
+    window.sessionStorage.setItem("file-item-focus", focusedElement.id);
+  } else {
+    window.sessionStorage.removeItem("file-item-focus");
   }
-  window.sessionStorage.setItem("file-item-focus-index", focusIndex.toString());
 }
 
 function updateFocus() {
-  const focusIndex = window.sessionStorage.getItem("file-item-focus-index") ?? -1;
-  const fileItemElements = document.getElementsByClassName("file-item");
-  if (focusIndex > -1) {
-    fileItemElements[focusIndex].children[0].focus();
+  const focusItemId = window.sessionStorage.getItem("file-item-focus");
+  if (focusItemId) {
+    document.getElementById(focusItemId).children[0].focus();
   }
-  window.sessionStorage.setItem("file-item-focus-index", "-1");
+
+  window.sessionStorage.removeItem("file-item-focus");
+}
+
+function initializeFocus() {
+  const focusItemId = window.sessionStorage.getItem("file-item-focus");
+  if (focusItemId) {
+    document.write(`<style>
+    .initial-state #${focusItemId} {
+      background-color: var(--color-file-item-active-background) !important;
+      color: var(--color-file-item-active-foreground) !important;
+      outline: 0.0625rem solid var(--color-file-item-active-border) !important;
+    } </style>
+  `);
+  }
 }
