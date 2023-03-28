cf9faeb1275d077fa0f563d45348f0a471145c09
RoqueC <roquecarrizof@gmail.com>
2023-03-28 14:46:08 +0200

    Add version json

---
+++ scripts/set-version.ps1
@@ -0,0 +1,16 @@
+# Get the version and commit hash from GitHub Actions environment variables
+$version = $env:GITHUB_REF_NAME
+$commit = $env:GITHUB_SHA
+
+# Set the path to the JSON file to be updated
+$jsonFilePath = "../src/version.json"
+
+# Read the contents of the JSON file
+$jsonContent = Get-Content $jsonFilePath -Raw | ConvertFrom-Json
+
+# Replace the placeholders with the actual values
+$jsonContent.version = $jsonContent.version -replace "__VERSION__", $version
+$jsonContent.commit = $jsonContent.commit -replace "__COMMIT__", $commit
+
+# Write the updated JSON content back to the file
+$jsonContent | ConvertTo-Json -Depth 100 | Set-Content $jsonFilePath
--- src/assets/js/initial-state.js
+++ src/assets/js/initial-state.js
@@ -1,9 +1,5 @@
 window.addEventListener("load", removeInitialState);
 
 function removeInitialState() {
-  setTimeout(test, 1000)
-}
-
-function test() {
   document.body.classList.remove("initial-state");
 }
