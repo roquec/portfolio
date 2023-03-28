2cb4f828eea176039a6b993fa42aa3f7bdad1ba7
RoqueC <roquecarrizof@gmail.com>
2023-03-28 15:01:03 +0200

    Fix

--- scripts/set-version.ps1
+++ scripts/set-version.ps1
@@ -11,10 +11,10 @@ $jsonFilePath = "./src/version.json"
 $jsonContent = Get-Content $jsonFilePath -Raw | ConvertFrom-Json
 
 # Replace the placeholders with the actual values
-$jsonContent.version = $jsonContent.version -replace "__TIME__", $time
-$jsonContent.version = $jsonContent.version -replace "__BRANCH__", $branch
+$jsonContent.timestamp = $jsonContent.timestamp -replace "__TIME__", $time
+$jsonContent.branch = $jsonContent.branch -replace "__BRANCH__", $branch
 $jsonContent.commit = $jsonContent.commit -replace "__COMMIT__", $commit
-$jsonContent.commit = $jsonContent.commit -replace "__BUILD__", $build
+$jsonContent.build = $jsonContent.build -replace "__BUILD__", $build
 
 # Write the updated JSON content back to the file
 $jsonContent | ConvertTo-Json -Depth 100 | Set-Content $jsonFilePath
