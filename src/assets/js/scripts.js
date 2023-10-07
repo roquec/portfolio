1306bff446cc6ac405446a0356c28b56da593faf
RoqueC <roquecarrizof@gmail.com>
2023-10-07 14:05:21 +0200

    add favicon

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,11 @@
+---
+layout: none
+---
+
+{% include _js/theme-selector.js %}
+{% include _js/folder-collapse.js %}
+{% include _js/file-item-focus-state.js %}
+{% include _js/navigation-panel.js %}
+{% include _js/panel-resize.js %}
+{% include _js/initial-state.js %}
+{% include _js/search-function.js %}
