8f84b322e98cb570e994c435c9ceb3b36ee5007a
RoqueC <roquecarrizof@gmail.com>
2023-10-07 20:37:01 +0200

    Print lighthouse report

---
+++ .github/util/print-lighthouse-report.ps1
@@ -0,0 +1,20 @@
+# Get lighthouse report values
+$performance = ${{ fromJSON(steps.lighthouse.outputs.manifest)[0].summary.performance }}
+$accessibility = ${{ fromJSON(steps.lighthouse.outputs.manifest)[0].summary.accessibility }}
+$bestPractices = ${{ fromJSON(steps.lighthouse.outputs.manifest)[0].summary.best-practices }}
+$seo = ${{ fromJSON(steps.lighthouse.outputs.manifest)[0].summary.seo  }}
+
+# Format summary
+$summary = @'
+## ⚡️🏠 Lighthouse report
+See full report [here](${reportUrl}). Here's the summary:
+| Category | Score |
+| -------- | ----- |
+| Performance | $($performance) |
+| Accessibility | $($accessibility) |
+| Best-practices | $($bestPractices) |
+| SEO | $($seo) |
+'@
+
+# Write to environment file
+echo $summary >> $GITHUB_STEP_SUMMARY
