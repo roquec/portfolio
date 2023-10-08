dd12e89eab9f98cb23ff1073f5ac868bca1fe898
RoqueC <roquecarrizof@gmail.com>
2023-10-08 02:46:50 +0200

    test

---
+++ .github/util/update-gist.ps1
@@ -0,0 +1,106 @@
+$gist_id = $env:UPDATE_GIST_ID
+$path = $env:UPDATE_GIST_PATH
+$token = $env:UPDATE_GIST_TOKEN
+
+# Checking source
+if (-not (Test-Path -Path $path -PathType Container)) {
+    throw "Invalid source path. $path is not a directory."
+}
+
+# List files on source
+$source_list = Get-ChildItem -Path $path
+$source_files = @()
+
+foreach ($file in $source_list) {
+  if (-not($file.PSIsContainer)) {
+    $source_files.Add($file.Name)
+  }
+}
+
+# Prepare files content
+foreach ($file in $source_files) {
+    try {
+        # Read file content with UTF-8 encoding to avoid binary files
+        $content = Get-Content -Path (Join-Path -Path $path -ChildPath $file) -Raw -Encoding UTF8
+    }
+    catch {
+        $source_files.Remove($file)
+    }
+}
+
+Write-Host "Found $($source_files.Count) readable files from source: $($source_files)"
+
+# Define gist git
+$gist_git_url = "https://$($token)@gist.github.com/$($gist_id).git"
+$gist_git_dir = "gist-clones\$($gist_id)"
+
+# Reset working directory
+if (Test-Path -Path $gist_git_dir -PathType Container) {
+    Remove-Item -Path $gist_git_dir -Recurse -Force
+}
+elseif (Test-Path -Path $gist_git_dir -PathType Leaf) {
+    Remove-Item -Path $gist_git_dir -Force
+}
+
+try {
+    # Start cloning gist
+    # Can't use API, as API only limits to 300 files in list, and more importantly can't add new file
+    # https://docs.github.com/en/rest/gists/gists?apiVersion=2022-11-28#about-gists
+    git clone $gist_git_url $gist_git_dir --depth 1
+
+    $target_files = Get-ChildItem -Path $gist_git_dir
+    # Since it's Git, skip .git directory
+    if ($target_files -contains ".git") {
+        $target_files.Remove(".git")
+    }
+
+    Write-Host "Found $($target_files.Count) files in gist"
+
+    # Files to remove
+    $removed_files = @()
+    foreach ($file in $target_files) {
+        if ($file.Name -notin $source_files) {
+            Remove-Item -Path (Join-Path -Path $gist_git_dir -ChildPath $file.Name) -Force
+            $removed_files.Add($file.Name)
+        }
+    }
+
+    Write-Host "Removed $($removed_files.Count) files from Gist repo: $($removed_files)"
+
+    # Update or add
+    foreach ($file in $source_files) {
+        # Basically copy and replace
+        if (Test-Path -Path (Join-Path -Path $gist_git_dir -ChildPath $file)) {
+            Remove-Item -Path (Join-Path -Path $gist_git_dir -ChildPath $file) -Force
+        }
+        Copy-Item -Path (Join-Path -Path $path -ChildPath $file) -Destination (Join-Path -Path $gist_git_dir -ChildPath $file) -Force
+        Write-Host "Copied $file"
+    }
+
+    $gist_repo = Get-GitRepository -Repository $gist_git_dir
+
+    Set-GitConfig -Repository $gist_repo -Name "user.email" -Value $env:GITHUB_ACTOR
+    Set-GitConfig -Repository $gist_repo -Name "user.name" -Value "$($env:GITHUB_ACTOR)@users.noreply.github.com"
+
+    if ($removed_files.Count -gt 0) {
+        Remove-GitIndex -Repository $gist_repo -Path $removed_files
+    }
+
+    if ($source_files.Count -gt 0) {
+        Add-GitIndex -Repository $gist_repo -Path $source_files
+    }
+
+    # No changes detected, exit with "success-ish"
+    if (-not $gist_repo.IsDirty) {
+        Write-Host "No changes detected in Gist repo."
+        exit 0
+    }
+
+    $gist_repo.Commit("Sync from repo by $($env:GITHUB_ACTOR), ref: $($env:GITHUB_REF).")
+    $gist_repo.Remote.Push()
+
+    Write-Host "Pushed changes to Gist repo."
+}
+catch {
+    throw "Unable to work with gist git"
+}
