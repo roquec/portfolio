64b6760fb6cd81898a57dc758c7211c1f68ef099
RoqueC <roquecarrizof@gmail.com>
2023-10-13 16:48:21 +0200

    test

--- src/_js/modules/focus.js
+++ src/_js/modules/focus.js
@@ -19,10 +19,22 @@ class Focus {
     return this;
   }
 
+  #setInitialStateStyles(focusItemId) {
+    let style = `
+    <style>
+      .initial-state #${CSS.escape(focusItemId)} {
+        background-color: var(--color-file-active-background) !important;
+        color: var(--color-file-active-foreground) !important;
+        outline: 0.0625rem solid var(--color-file-active-border) !important;
+      }
+    </style>`;
+    document.head.insertAdjacentHTML("beforeend", style);
+  }
+
   #onDomReady() {
     this.#applyState();
     //window.addEventListener("focusin", (event) => this.#onFocus(event));
-    document.addEventListener("click", (event) => this.#onItemMouseUp(event));
+    document.addEventListener("mousedown", (event) => this.#onItemMouseUp(event));
   }
 
   #onPageReady() {
@@ -38,37 +50,25 @@ class Focus {
   #applyState() {
     const focusItemId = window.sessionStorage.getItem(Focus.FOCUS_STORAGE_KEY);
     if (focusItemId) {
-      //document.getElementById(focusItemId).classList.add("focused");
-      document.getElementById(focusItemId).children[0].focus();
-      window.sessionStorage.removeItem(Focus.FOCUS_STORAGE_KEY);
+      document.getElementById(focusItemId).classList.add("focused");
+      //document.getElementById(focusItemId).children[0].focus();
+      //window.sessionStorage.removeItem(Focus.FOCUS_STORAGE_KEY);
     }
   }
 
-  #setInitialStateStyles(focusItemId) {
-    let style = `
-    <style>
-      .initial-state #${CSS.escape(focusItemId)} {
-        background-color: var(--color-file-active-background) !important;
-        color: var(--color-file-active-foreground) !important;
-        outline: 0.0625rem solid var(--color-file-active-border) !important;
-      }
-      .initial-state #${CSS.escape(focusItemId)}:hover {
-        background-color: var(--color-file-active-background) !important;
-        color: var(--color-file-active-foreground) !important;
-        outline: 0.0625rem solid var(--color-file-active-border) !important;
-      }
-    </style>`;
-    document.head.insertAdjacentHTML("beforeend", style);
-  }
 
   #onItemMouseUp(event) {
+    const focusItemId = window.sessionStorage.getItem(Focus.FOCUS_STORAGE_KEY);
+    document.getElementById(focusItemId).classList.remove("focused");
+
     if (event.target.parentElement.classList.contains("file-item")) {
       window.sessionStorage.setItem(Focus.FOCUS_STORAGE_KEY, event.target.parentElement.id);
+      event.target.parentElement.classList.add("focused");
     }
   }
 
   #onFocus(event) {
-    console.log("FOCUS");
+    //console.log("FOCUS");
 
     //if (event.target.parentElement.id !== this.focusedElement.id) {
     //  this.focusedElement?.classList.remove("focused");
